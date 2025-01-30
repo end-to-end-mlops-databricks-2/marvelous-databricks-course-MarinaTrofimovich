12ddedd1e8a8515eba10de89a617751a88b2c323
Trafimovich <m.trafimovich@house-of-communication.com>
2025-01-30 12:41:21 +0100

    tempalte

---
+++ scripts/process_data.py
@@ -0,0 +1,26 @@
+import yaml
+
+from sales_predictor.data_processor import DataProcessor
+from sales_predictor.config import ProjectConfig
+
+# Load configuration
+config = ProjectConfig.from_yaml(config_path="../../project_config.yml")
+
+print("Configuration loaded:")
+print(yaml.dump(config, default_flow_style=False))
+
+# COMMAND ----------
+# Initialize DataProcessor
+data_processor = DataProcessor("../data/data.csv", config)
+
+# Preprocess the data
+data_processor.preprocess_data()
+
+# COMMAND ----------
+# Split the data
+X_train, X_test = data_processor.split_data()
+
+print("Training set shape:", X_train.shape)
+print("Test set shape:", X_test.shape)
+
+data_processor.save_to_catalog(X_train, X_test)
