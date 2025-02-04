b8cd6979adbf8bed031a65c7d282d530a83789df
Trafimovich <m.trafimovich@house-of-communication.com>
2025-02-04 12:29:46 +0100

    process_data.py change paths

--- scripts/process_data.py
+++ scripts/process_data.py
@@ -1,19 +1,25 @@
-#%pip install /Volumes/mlops_dev/mtrofimo/churn_predictor/churn_predictor-0.0.1-py3-none-any.whl
+%pip install /Volumes/mlops_dev/mtrofimo/churn_predictor/churn_predictor-0.0.1-py3-none-any.whl
 
 import yaml
+import os
 
 from churn_predictor.data_processor import DataProcessor
 from churn_predictor.config import ProjectConfig
 
 # Load configuration
-config = ProjectConfig.from_yaml(config_path="../project_config.yml")
+config_path = os.path.abspath("project_config.yml")
+print('config_path:', config_path)
+config = ProjectConfig.from_yaml(config_path=config_path)
+#config = ProjectConfig.from_yaml(config_path="../project_config.yml")
 
 print("Configuration loaded:")
 print(yaml.dump(config, default_flow_style=False))
 
 # COMMAND ----------
 # Initialize DataProcessor
-data_processor = DataProcessor("../data/data.csv", config)
+data_path = os.path.abspath("data/data.csv")
+data_processor = DataProcessor(data_path, config)
+#data_processor = DataProcessor("../data/data.csv", config)
 
 # Preprocess the data
 data_processor.preprocess_data()
