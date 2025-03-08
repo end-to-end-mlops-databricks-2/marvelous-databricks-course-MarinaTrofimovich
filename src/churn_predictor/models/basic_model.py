cd6348ab5055af8de2ad3ace4642c4d71df1265c
MarinaTrofimovich <MarinaTrofimovich>
2025-03-08 13:36:27 +0000

    fix basic model

--- src/churn_predictor/models/basic_model.py
+++ src/churn_predictor/models/basic_model.py
@@ -209,7 +209,7 @@ class BasicModel:
         current_model_uri = f"runs:/{self.run_id}/lightgbm-pipeline-model"
         current_model = mlflow.sklearn.load_model(current_model_uri)
         logger.info("✅ Model successfully loaded.")
-        predictions_current = model.predict(X_test)
+        predictions_current = current_model.predict(X_test)
         predictions_current = predictions_current.rename(columns={"prediction": "prediction_current"})
         logger.info("predictions_current")
         logger.info(predictions_current)
